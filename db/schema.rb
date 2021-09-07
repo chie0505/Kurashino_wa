3fff72581203c51b567b89af98ffb931cad641c0
Chie0505 <adtnn.50@gmail.com>
2021-09-07 14:40:54 +0000

    [Update]投稿機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_122336) do
+ActiveRecord::Schema.define(version: 2021_09_07_141237) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_122336) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_photo", default: "default_profile_img.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
